55fcff3ac02f931b81d91c9c2bc7b250d2ef2b3b
Luis Lopez Sanchez <luisls2129@gmail.com>
2020-10-06 11:20:15 +0200

    modificacion del ej1 y añadido el 2

--- ejercicios4.1/ejercicio1.php
+++ ejercicios4.1/ejercicio1.php
@@ -5,22 +5,20 @@ class Empleado{
     private $nombre;
     private $sueldo;
 
-    function __construct($nombre, $sueldo){
-        $this -> nombre = $nombre;
-        $this -> sueldo = $sueldo;
-        echo $this-> nombre . " tiene un sueldo de " . $this -> sueldo . "<br>";
+    function __construct(){
+        
     }
 
     public function getNombre(){
-        echo $this->nombre;
+        return $this->nombre;
     }
 
     public function setNombre($nombre){
         $this->nombre = $nombre;
     }
 
-    public function getSuelto(){
-        echo $this -> sueldo;
+    public function getSueldo(){
+        return $this -> sueldo;
     }
 
     public function setSueldo($sueldo){
@@ -29,6 +27,13 @@ class Empleado{
     
 
 }
- 
-$empleado1 = new Empleado("Luis", 150);
-$empleado1 = new Empleado("Ruben", 500);
+
+$empleado1 = new Empleado();
+$empleado1->setNombre("Luis");
+$empleado1->setSueldo(150);
+$empleado2 = new Empleado();
+$empleado2->setNombre("Ruben");
+$empleado2->setSueldo(500);
+
+echo $empleado1 -> getNombre() . " tiene un sueldo de " . $empleado1->getsueldo() . "<br>";
+echo $empleado2 -> getNombre() . " tiene un sueldo de " . $empleado2 -> getSueldo() . "<br>";
---
+++ ejercicios4.1/ejercicio2.php
@@ -0,0 +1,26 @@
+<?php
+
+class Empleado{
+    
+    private $nombre;
+    private $sueldo;
+
+    function __construct($nombre, $sueldo){
+        $this -> nombre = $nombre;
+        $this -> sueldo = $sueldo;
+        echo $this-> nombre . " tiene un sueldo de " . $this -> sueldo . "<br>";
+    }
+
+    public function getNombre(){
+        echo $this->nombre;
+    }
+
+    public function getSuelto(){
+        echo $this -> sueldo;
+    }
+    
+
+}
+ 
+$empleado1 = new Empleado("Luis", 150);
+$empleado1 = new Empleado("Ruben", 500);
